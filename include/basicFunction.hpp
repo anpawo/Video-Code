5182ae2fb628e3ff25a4aef94b874dc1924d579d
Marius Rousset <marius.rousset@epitech.eu>
2024-10-28 10:43:59 +0100

    feat: overlay function

--- include/basicFunction.hpp
+++ include/basicFunction.hpp
@@ -9,4 +9,13 @@
 
 #include <string>
 
+/**
+ * @brief concatenates 2 videos. the second video will be starting at the end of the first video.
+
+ * @param v1 first video
+ * @param v2 second video
+ * @param output name of the output video
+ */
 void concatenateVideo(std::string v1, std::string v2, std::string output = "output.mp4");
+
+void overlayVideo(std::string main, std::string overlay, std::string output = "output.mp4");
