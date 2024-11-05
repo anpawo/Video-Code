0227367500718d8536cd764ed2c892e3b1af1541
Marius Rousset <marius.rousset@epitech.eu>
2024-11-05 19:50:09 +0100

    feat: interface and abstract class for the filters

--- include/Filter/_AFilter.hpp
+++ include/Filter/_AFilter.hpp
@@ -8,10 +8,38 @@
 #pragma once
 
 #include "Filter/_IFilter.hpp"
+#include "Utils/Vector.hpp"
 
 class AFilterComplex : public IFilter {
 public:
 
-    AFilterComplex() = default;
+    AFilterComplex(std::string&& stream0, std::string&& stream1, std::string&& output) : _stream0(stream0), _stream1(stream1), _output(output) {}
+
     virtual ~AFilterComplex() = default;
+
+    virtual AFilterComplex& set()
+    {
+        return *this;
+    }
+
+    virtual std::string getNewInputs()
+    {
+        return _output;
+    }
+
+protected:
+
+    const std::string getInputName(const std::vector<std::string>& defaultInputStreams, const std::vector<std::string>& newInputStreams, const std::string& streamName) const
+    {
+        try {
+            return "[" + std::to_string(findIndex(defaultInputStreams, streamName)) + ":v:0]";
+        } catch (const Error&) {
+            find(newInputStreams, streamName);
+            return "[" + streamName + "]";
+        }
+    }
+
+    std::string _stream0;
+    std::string _stream1;
+    std::string _output;
 };
--- include/Filter/_IFilter.hpp
+++ include/Filter/_IFilter.hpp
@@ -17,14 +17,19 @@ public:
     virtual ~IFilter() = default;
 
     /**
+     * @brief sets the arguments you will use in the command. to each it's own.
+     */
+    virtual IFilter& set() = 0;
+
+    /**
      * @brief return the formatted command string.
      */
-    virtual std::string getCommand() = 0;
+    virtual std::string getCommand(const std::vector<std::string>& defaultInputStreams, const std::vector<std::string>& newInputStreams) = 0;
 
     /**
      * @brief return the new input streams created by the complex filter.
      */
-    virtual std::vector<std::string> getNewInputs() = 0;
+    virtual std::string getNewInputs() = 0;
 
 private:
 };
