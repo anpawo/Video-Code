ef5de765e74e56c1a07f38ccaba84c17f8ebb5e2
Marius Rousset <marius.rousset@epitech.eu>
2025-01-26 19:40:35 +0100

    feat(Python Source File): function declaration of the Transformations for the python frontend

---
+++ python/transformation/Transformation.pyi
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+class Transformation:
+    """
+    Represents a `Transformation`. ::
+
+        def move() -> Transformation: ...
+        def zoom() -> Transformation: ...
+
+    """
---
+++ python/transformation/Translate.pyi
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from Transformation import Transformation
+
+def translate(x: int, y: int) -> Transformation:
+    """
+    Moves a frame.
+    """
+    ...
