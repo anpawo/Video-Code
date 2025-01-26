fe97dc906f88bd6cdffdeaadf27fb002801ee9eb
Marius Rousset <marius.rousset@epitech.eu>
2025-01-26 19:35:15 +0100

    feat(PythonAPI): encapsulation of the Python API into C++.

---
+++ include/python/API.hpp
@@ -0,0 +1,127 @@
+/*
+** EPITECH PROJECT, 2025
+** video-code
+** File description:
+** API
+*/
+
+#pragma once
+
+#include <cstdio>
+#include <utility>
+
+#include "utils/Exception.hpp"
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <string>
+
+namespace python::API
+{
+    // Convert the C++ value to a PyObject.
+    inline PyObject* convertToPythonObject(std::string&& arg)
+    {
+        return PyUnicode_FromString(arg.c_str());
+    }
+
+    inline PyObject* convertToPythonObject(const char*&& arg)
+    {
+        return PyUnicode_FromString(arg);
+    }
+
+    inline PyObject* convertToPythonObject(bool&& arg)
+    {
+        return PyBool_FromLong(arg);
+    }
+
+    inline PyObject* convertToPythonObject(int&& arg)
+    {
+        return PyLong_FromLong(arg);
+    }
+
+    inline PyObject* convertToPythonObject(double&& arg)
+    {
+        return PyFloat_FromDouble(arg);
+    }
+
+    inline PyObject* convertToPythonObject(float&& arg)
+    {
+        return PyFloat_FromDouble(arg);
+    }
+
+    // Convert the PyObject to C++ value.
+    template <typename RetTy>
+    RetTy convertToCppObject(PyObject* o)
+    {
+        if constexpr (std::is_same_v<RetTy, int>) {
+            return PyLong_AsLong(o);
+        } else if constexpr (std::is_same_v<RetTy, double> || std::is_same_v<RetTy, float>) {
+            return PyFloat_AsDouble(o);
+        } else if constexpr (std::is_same_v<RetTy, std::string>) {
+            return PyUnicode_AsUTF8(o);
+        }
+    }
+
+    template <typename RetTy, typename... ArgsTy>
+    RetTy call(std::string&& moduleName, std::string&& functionName, ArgsTy... args)
+    {
+        // Initialize the Python API
+        Py_Initialize();
+
+        // Current path added to the Import Path
+        PyRun_SimpleString(
+            "import sys\n"
+            "sys.path.append('./src/python')"
+        );
+
+        // Import the Module (file name)
+        PyObject* pModule = PyImport_ImportModule(moduleName.c_str());
+
+        if (pModule) {
+            // Get the function from the module
+            PyObject* pFunc = PyObject_GetAttrString(pModule, functionName.c_str());
+
+            if (PyCallable_Check(pFunc)) {
+                // PyObject* pArgs = PyTuple_Pack(1, PyUnicode_FromString("python/ast_input.py"));
+
+                // Create an empty Tuple of size len(args)
+                PyObject* pArgs = PyTuple_New(sizeof...(args));
+
+                // Convert C++ arguments to Python objects
+                size_t i = 0;
+                ((PyTuple_SetItem(pArgs, i++, convertToPythonObject(std::forward<ArgsTy>(args)))), ...);
+
+                // Call the function
+                PyObject* pValue = PyObject_CallObject(pFunc, pArgs);
+
+                Py_XDECREF(pArgs);
+
+                if (!pValue) {
+                    PyErr_Print();
+                    throw Error("Function '" + functionName + "' failed.");
+                }
+
+                if constexpr (!std::is_same_v<RetTy, void>) {
+                    RetTy result = convertToCppObject<RetTy>(pValue);
+                    Py_DECREF(pValue);
+                    return result;
+                }
+
+                Py_XDECREF(pValue);
+
+                // Finalize the Python API
+                Py_Finalize();
+            } else {
+                throw Error("Python function '" + functionName + "' is not callable.");
+            }
+
+            Py_XDECREF(pFunc);
+            Py_XDECREF(pModule);
+        } else {
+            PyErr_Print();
+            throw Error("Failed to load Python module: '" + moduleName + "'.");
+        }
+
+        // Finalize the Python interpreter
+    }
+};
