a8b51893693610d44166380b5a2921f7d3d8e422
Marius Rousset <marius.rousset@epitech.eu>
2025-01-27 13:53:58 +0100

    feat: handle errors and new Node: Subscript and Slice

--- src/python/serialise.py
+++ src/python/serialise.py
@@ -8,6 +8,8 @@ from typing import Any
 
 
 def unparseCall(x: ast.Call) -> list:
+    if type(x.func) == ast.Attribute:
+        return [type(x).__name__, x.func.attr, [unparse(x.func.value)] + list(map(unparse, x.args))]
     return [type(x).__name__, unparse(x.func), list(map(unparse, x.args))]
 
 
@@ -15,11 +17,21 @@ def unparseAssign(x: ast.Assign) -> list:
     return [type(x).__name__, unparse(x.targets[0]), unparse(x.value)]
 
 
+def unparseSubscript(x: ast.Subscript) -> list:
+    return [type(x).__name__, unparse(x.value), unparse(x.slice)]
+
+
+def unparseSlice(x: ast.Slice) -> list:
+    return [unparse(x.lower), unparse(x.upper)]
+
+
 def unparse(x: Any) -> Any:
     mapUnparse = {
         ast.Expr: lambda x: unparse(x.value),
         ast.Call: unparseCall,
         ast.Assign: unparseAssign,
+        ast.Subscript: unparseSubscript,
+        ast.Slice: unparseSlice,
         ast.Constant: lambda x: x.value,
         ast.Name: lambda x: x.id,
         list: lambda x: list(map(unparse, x)),
@@ -60,7 +72,16 @@ def toJson(filepath: str) -> str:
         content = commentsToLabel(file.read())
 
     # Parse the file content into an AST
-    parsedAst = unparseAst(ast.parse(content))
+    parsedAst = ast.parse(content)
+    # print(f"parsedAst=[{ast.dump(parsedAst, indent=4)}]")
 
-    # Convert the AST body into a dictionary and print it
-    return json.dumps(parsedAst)
+    # Parse the ast and convert it to a list of instructions
+    unparsedAst = unparseAst(parsedAst)
+    # print(f"unparsedAst=[{unparsedAst}]")
+
+    # Serialize the instructions to JSON
+    return json.dumps(unparsedAst)
+
+
+if __name__ == "__main__":
+    print(toJson("video.py"))
