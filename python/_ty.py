f10f39d90596b9a81a6f65d339c94864828992d1
Marius Rousset <marius.rousset@epitech.eu>
2025-01-28 02:56:24 +0100

    feat(Python frontend): type annotations

---
+++ python/_ty.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#
+# Types
+#
+
+from typing import Annotated, Literal, TypeAlias, TypeVar, Union
+
+
+# unsigned int
+uint = Annotated[int, "unsigned"]
+
+
+# sides
+LEFT = 0
+RIGHT = 1
+UP = 2
+DOWN = 3
+ALL = 4
+
+side = Literal[0] | Literal[1] | Literal[2] | Literal[3] | Literal[4]
