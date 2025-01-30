542ad019fe94cc0b309150857097b8c3ea9637f0
Marius Rousset <marius.rousset@epitech.eu>
2025-01-30 23:30:14 +0100

    feat(window): set default background color to black

--- src/vm/WindowEvent.cpp
+++ src/vm/WindowEvent.cpp
@@ -12,6 +12,7 @@ WindowEvent::WindowEvent(const std::map<int, std::function<void()>> &events, std
     , _events(events)
 {
     ///< Define the settings of the window
+    setStyleSheet("background-color: black;");
     setWindowTitle(("Video-Code  |  " + sourceFile).c_str());
     move(width / 2, 0);
     resize(width / 2, height / 2);
