96dcc05dd5fbe1056c2f93e06a936b8e79ad700d
Marius Rousset <marius.rousset@epitech.eu>
2025-02-01 02:56:13 +0100

    test: overlay example (working)

--- video.py
+++ video.py
@@ -1,12 +1,9 @@
 from python.VideoCode import *
 
-v1 = video("video/v.mp4")[0]
+v1 = video("video/v.mp4")
 v2 = v1.copy()
 
 v2.apply(translate(50, 50))
-
-# v1 frame
-v1.add()
 
 # overlay
 v1.overlay(v2).add()
