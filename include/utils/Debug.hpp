0f7685b34c34d1ddb46a9b1cb9f9016a0ed0ef67
Marius Rousset <marius.rousset@epitech.eu>
2025-02-18 21:42:08 +0100

    feat: debug macro

---
+++ include/utils/Debug.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** video-code
+** File description:
+** Debug
+*/
+
+#pragma once
+
+#define VC_DEBUG_ON
+
+#include <iostream>
+
+#ifdef VC_DEBUG_ON
+    #define VC_LOG_DEBUG(x)              \
+        do                               \
+        {                                \
+            std::cout << x << std::endl; \
+        } while (0);
+#else
+    #define VC_LOG_DEBUG(x)
+#endif
