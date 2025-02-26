d6e34976a209c0580dd1472f751f6a3a6e3ac895
Marius Rousset <marius.rousset@epitech.eu>
2025-02-26 16:54:10 +0100

    update: video presentation

--- video.py
+++ video.py
@@ -3,7 +3,5 @@ from frontend.VideoCode import *
 
 v1 = video("video/v.mp4")
 
-v1[:20].apply(fadeIn())
-v1[-20:].apply(fadeOut())
-
-v1.add()
+v1[0:20].apply(fadeIn()).add()
+v1[20:40].apply(fadeOut()).add()
