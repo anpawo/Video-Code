99522a580d22132bee52979b6bb52a0dd16ab90b
Hippolyte lefer <hippolytelefer@gmail.com>
2025-03-18 18:34:40 +0100

    feat: enhance test script output with color-coded success and failure messages

--- test/fonctionnal/run_test.sh
+++ test/fonctionnal/run_test.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+GREEN="\e[32m"
+RED="\e[31m"
+
 SCRIPT_DIR="test/fonctionnal/scripts"
 
 for FILE in "$SCRIPT_DIR"/*; do
   if [[ -f "$FILE" ]]; then
-    echo -n "Processing $FILE... "
+    printf "%-50s" "$FILE"
     ./video-code --sourceFile "$FILE" --generate test_out.mp4 > /dev/null 2>&1
     if [[ $? -eq 0 && -f "test_out.mp4" ]]; then
       rm "test_out.mp4"
-      echo "Success"
+      printf "${GREEN} [OK] ${NC}\n"
     else
-      echo "Failed"
+      printf "${RED} [KO] ${NC}\n"
     fi
   fi
 done
