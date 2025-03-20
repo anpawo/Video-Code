1d819007e56686ac96d4b7d367f9a0ffac94a0b8
Hippolyte lefer <hippolytelefer@gmail.com>
2025-03-20 10:46:14 +0100

    feat: update scale transformation to use centered parameter instead of mode

--- videocode/transformation/other/Scale.py
+++ videocode/transformation/other/Scale.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from videocode.transformation.Transformation import Transformation
-from videocode.Constant import mode
 
 class scale(Transformation):
     """
@@ -11,7 +10,7 @@ class scale(Transformation):
     def __init__(self, *, factor: float | int | tuple[float, float] = 2, centered : bool = False) -> None:
         """
         :param factor: The scale factor. If a tuple, the first value is the x scale and the second value is the y scale.
-        :param mode: The mode of the scale. Can be "Origin" or "Center".
+        :param centered: If True, the scale is centered. If False, the scale is applied from the top left corner.
         """
         if isinstance(factor, tuple):
             self.factor = factor
