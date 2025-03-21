9a59305a52d3fd290972e09e08ce3e7bb5a84c10
Hippolyte lefer <hippolytelefer@gmail.com>
2025-03-21 11:08:19 +0100

    feat: presentation video

--- video.py
+++ video.py
@@ -3,13 +3,47 @@ from videocode.VideoCode import *
 x = 700
 y = 10
 
-t = text("Hello", fontSize=3, duration=2).apply(translate(x, y + 175))
-t.apply(fadeIn())
-t.apply(fadeOut(), zoom(factor=(1, 3)))
+t = text("Video-Code", fontSize=3, duration=2).apply(translate(x, y))
+t.apply(fadeIn(sides=LEFT), endTime=1)
+t.apply(fadeOut(sides=RIGHT), startTime=1)
 t.add()
 
-v = video("video/v.mp4").apply(translate(x, y + 175))
-v.apply(scale(factor=(1, 0.2), centered=True), endTime=2)
-v.apply(scale(factor=(0.2, 1), centered=True), startTime=2)
-v.add()
-v.keep()
+t = text("made by", fontSize=3, duration=2).apply(translate(x, y))
+t.apply(fadeIn(sides=LEFT), endTime=1)
+t.apply(fadeOut(sides=RIGHT), startTime=1)
+t.add()
+
+t = text("Hippolyte and Marius", fontSize=3, duration=2).apply(translate(450, y))
+t.apply(fadeIn(sides=LEFT), endTime=1)
+t.apply(fadeOut(sides=RIGHT), startTime=1)
+t.add()
+
+t = text("3 different inputs", fontSize=3, duration=2).apply(translate(450, y))
+t.apply(fadeIn(sides=LEFT), endTime=1)
+t.apply(fadeOut(sides=RIGHT), startTime=1)
+t.add()
+
+t = text("text", fontSize=3, duration=2).apply(translate(x, y- 200))
+t.add()
+
+t = image("video/image.png").apply(translate(x, y-200))
+t.add()
+
+t = video("video/v.mp4").apply(translate(x, y-200))
+t.add()
+
+t = text("6 different effects", fontSize=3, duration=2).apply(translate(x, y))
+t.add()
+
+t = image("video/image.png").apply(translate(x, y))
+t.apply(fadeIn(sides=LEFT), endTime=1)
+t.apply(fadeOut(sides=RIGHT), startTime=1)
+t.add()
+
+t = video("video/v.mp4").apply(translate(x, y))
+t.apply(grayscale(), move(x=0, y=0), endTime=1)
+t.apply(zoom(factor=(1, 3)), startTime=1, endTime=3)
+t.apply(zoom(factor=(3, 1)), startTime=3, endTime=6)
+t.apply(scale(factor=(1, 3)), startTime=6, endTime=7)
+t.apply(zoom(factor=(3, 1)), startTime=7)
+t.add()
