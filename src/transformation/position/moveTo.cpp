069839b833b2a2afa853764a25552585620abd67
Marius Rousset <marius.rousset@epitech.eu>
2025-04-02 11:36:41 +0200

    fix: moveTo -> float to int conversion creating some difference in the result.

--- src/transformation/position/moveTo.cpp
+++ src/transformation/position/moveTo.cpp
@@ -22,10 +22,15 @@ void transformation::moveTo(std::shared_ptr<IInput>& input, const json::object_t
     float velX = dstX.is_null() ? 0 : ((dstX.get<int>() - srcX) / (duration - 1.0));
     float velY = dstY.is_null() ? 0 : ((dstY.get<int>() - srcY) / (duration - 1.0));
 
+    float currX = srcX;
+    float currY = srcY;
+
     for (size_t i = 1; i < duration; i++) {
-        input->addTransformation(i, [velX, velY](Frame& f) {
-            f.meta.position.x += velX;
-            f.meta.position.y += velY;
+        currX += velX;
+        currY += velY;
+        input->addTransformation(i, [currX, currY](Frame& f) {
+            f.meta.position.x = currX;
+            f.meta.position.y = currY;
         });
     }
 }
