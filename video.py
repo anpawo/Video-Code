466512dee11aea780649b0607990824d174533f6
Eliot GOURDOUX <eliotgourdoux@Eliots-MacBook-Air.local>
2026-01-13 16:38:10 +0100

    feat: contrast filter (Resolves #102)

--- video.py
+++ video.py
@@ -4,8 +4,8 @@
 from videocode.videocode import *
 
 # Display the same image three times: left lower contrast, center original, right higher contrast
-img_low_contrast = image("../test.png").position(x=-4, y=0).scale(0.5).apply(contrast(-80), duration=2)
+img_low_contrast = image("../test.png").position(x=-4, y=0).scale(0.5).apply(contrast(-50), duration=2)
 
 img_center = image("../test.png").position(x=0, y=0).scale(0.5)
 
-img_high_contrast = image("../test.png").position(x=4, y=0).scale(0.5).apply(contrast(80), duration=2)
+img_high_contrast = image("../test.png").position(x=4, y=0).scale(0.5).apply(contrast(50), duration=2)
